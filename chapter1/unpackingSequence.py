0f8a982db255dda85445724ce63ec25fbb8afdc5
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-07-26 15:23:05 +0300

    Initial commit

---
+++ chapter1/unpackingSequence.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+
+if __name__ == '__main__':
+    p = (4, 5)
+    x, y = p
+    print(x)
+    print(y)
+
+    data = ["ACME", 50, 91.1, (2012, 12, 21)]
+    name, shares, price, date = data
+    print(name)
+    print(shares)
+    print(price)
+    print(date)
+
+    word = "hello"
+    h, e, l, _, o = word
+    print(h)
+    print(e)
+    print(l)
+    print(o)
